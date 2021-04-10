97e78f560dfbe27801d80aebf0f3509e5b664166
Kim Sangyeob <yeob32@gmail.com>
2021-04-11 00:28:56 +0900

    # jpa querydsl example

--- src/main/kotlin/com/example/demo/domain/company/custom/CompanyRepositoryImpl.kt
+++ src/main/kotlin/com/example/demo/domain/company/custom/CompanyRepositoryImpl.kt
@@ -1,38 +1,51 @@
 package com.example.demo.domain.company.custom
 
 import com.example.demo.domain.company.Company
+import com.example.demo.domain.company.CompanyStatus
 import com.example.demo.domain.company.QCompany
+import com.example.demo.domain.company.QCompany.company
 import com.example.demo.domain.company.dto.CompanySearchContext
 import com.example.demo.domain.company.dto.SearchType
+import com.querydsl.core.types.dsl.BooleanExpression
 import com.querydsl.jpa.JPQLQuery
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.PageImpl
 import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport
 import org.springframework.stereotype.Repository
+import java.time.Instant
 
 @Repository
-class CompanyRepositoryImpl : QuerydslRepositorySupport(Company::class.java), CustomCompanyRepository {
+class CompanyRepositoryImpl :
+    QuerydslRepositorySupport(Company::class.java), CustomCompanyRepository {
 
     override fun search(search: CompanySearchContext, pageable: Pageable): Page<Company> {
         val company: QCompany = QCompany.company
 
         val jpaQuery: JPQLQuery<Company> = from(company)
 
-        search.searchKeyword?.let {
-            jpaQuery.where(
-                when (search.searchType) {
-                    SearchType.NAME -> company.name.eq(search.searchKeyword)
-                    SearchType.ADDRESS -> company.address.contains(search.searchKeyword)
-                    SearchType.PHONE -> company.phone.contains(search.searchKeyword)
-                }
-            )
-        }
-
-        jpaQuery.where(company.createdAt.between(search.startDateTime, search.endDateTime))
-        jpaQuery.where(company.status.eq(search.companyStatus))
+        jpaQuery.where(
+            eqStatus(search.companyStatus)?.or(
+                searchKeyword(
+                    search.searchType,
+                    search.searchKeyword
+                )?.and(betweenCreatedAt(search.startDateTime, search.endDateTime))
+            ),
+        )
 
         val companies = querydsl!!.applyPagination(pageable, jpaQuery).fetch()
         return PageImpl(companies, pageable, jpaQuery.fetchCount())
     }
+
+    private fun searchKeyword(type: SearchType, keyword: String?): BooleanExpression? = keyword?.let {
+        when (type) {
+            SearchType.NAME -> company.name.eq(keyword)
+            SearchType.ADDRESS -> company.address.contains(keyword)
+            SearchType.PHONE -> company.phone.contains(keyword)
+        }
+    }
+
+    private fun eqStatus(status: CompanyStatus?): BooleanExpression? = status?.let { company.status.eq(status) }
+    private fun betweenCreatedAt(startDateTime: Instant, endDateTime: Instant): BooleanExpression =
+        company.createdAt.between(startDateTime, endDateTime)
 }
